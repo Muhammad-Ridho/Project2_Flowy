8652133db308b5a40ec6db999058df7b59b467a5
= <ramadhanjenius1@gmail.com>
2021-11-17 09:33:59 +0700

    Flowy_Apps

---
+++ app/src/main/java/org/aplas/project2_flowy/WelcomeDaftarMenuBunga.java
@@ -0,0 +1,15 @@
+package org.aplas.project2_flowy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WelcomeDaftarMenuBunga extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_daftar_menu_bunga);
+
+    }
+}
